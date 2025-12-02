7397f11570e10150e550bbc5c3d820f01282863c
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2025-12-02 15:32:23 +0600

    footer added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({ children }) {
         <div className="min-h-screen max-w-6xl mx-auto mt-8">
           {children}
         </div>
-        <h2>footer</h2>
+        <Footer></Footer>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-center mt-32">
+    <div className="text-center mt-44">
       <h1 className="text-5xl font-bold">Welcome to Product Explorer</h1>
       <p className="text-gray-500 mt-4">Browse products easily</p>
     </div>
